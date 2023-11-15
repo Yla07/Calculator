949165e123e71629e68102fa304c16f5a9915063
Yla07 <bastianwic07@wp.pl>
2023-11-15 20:16:44 +0100

    Dodanie komentarzy

--- kalkulator.py
+++ kalkulator.py
@@ -1,12 +1,15 @@
-import tkinter as tk
+import tkinter as tk  # zaimportowanie biblioteki tkinter 
 
+# Funkcja odpowiadajaca za sprawdzanie jak przycisk zostal wcisniety
 def button_click(value):
     current_text = display_data.get()
     display_data.set(current_text + str(value))
 
+# Funkcja odpowiadajaca za usuniencie wszystkiego z zmiennej display_data
 def clear_command():
     display_data.set("")
 
+# Funkcja odpowiadajanca za obliczenie rownania
 def calculate():
     try:
         result = eval(display_data.get())
@@ -14,20 +17,25 @@ def calculate():
     except Exception as e:
         display_data.set("Error")
 
+# Utworzenie okna tkinter
 root = tk.Tk()
 root.title("Kalkulator")
 root.resizable(0, 0)
 root.config(background="black")
 
+# Zmienna przechowojaca rownanie 
 display_data = tk.StringVar()
 display_data.set("")
 
+# Zmienna wyswietlajaca rownanie 
 display = tk.Label(root, width=15, height=2, textvariable=display_data, background="black", foreground="white")
 display.grid(row=0, column=1)
 
+# Zmienna wyswietlajca przycisk usuwania 
 clear = tk.Button(root, width=15, height=2, text="AC", command=clear_command, foreground="black", background="red")
 clear.grid(row=4, column=0)
 
+# Lista liczb
 buttons = [
     "1", "2", "3",
     "4", "5", "6",
@@ -37,6 +45,7 @@ buttons = [
 row_val = 1
 col_val = 0
 
+# Funkcja odpowiadajca z tworzenie przyciskow liczb od 1 - 9
 for button_text in buttons:
     button = tk.Button(root, width=15, height=2, text=button_text, command=lambda bt=button_text: button_click(bt),  foreground="white", background="grey")
     button.grid(row=row_val, column=col_val)
@@ -45,7 +54,7 @@ for button_text in buttons:
         col_val = 0
         row_val += 1
 
-# Corrected placement for the "0" button
+# Przycisk 0
 button_0 = tk.Button(root, width=15, height=2, text="0", command=lambda bt="0": button_click(bt), foreground="white", background="grey")
 button_0.grid(row=4, column=1)
 
@@ -54,13 +63,14 @@ operators = ["+", "-", "*", "/"]
 row_val = 1
 col_val = 3
 
+# Funkcja odpowiadajanca za tworzenie przyciskow operatorow
 for operator in operators:
     button = tk.Button(root, width=7, height=2, text=operator, command=lambda op=operator: button_click(op),background="darkorange", foreground="black")
     button.grid(row=row_val, column=col_val)
     row_val += 1
 
-
+#Przycisk rowna sie
 equal = tk.Button(root, width=15, height=2, text="=", command=calculate, foreground="black", background="green")
 equal.grid(row=4, column=2)
 
-root.mainloop()
+root.mainloop()  # zamkniecie okna tkinter
